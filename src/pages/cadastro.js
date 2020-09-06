99ccd4887d5d9dd6edd4da55542753e4f32ef7e8
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-09-06 10:30:35 -0300

    Cria telas de login e de cadastro

---
+++ src/pages/cadastro.js
@@ -0,0 +1,225 @@
+import React, { useRef } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+} from "react-native";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as Crypto from "expo-crypto";
+import { Entypo } from "@expo/vector-icons";
+import CustomAlert from "../components/customAlert";
+import logo from "../../assets/logo.png";
+import { addUser } from "../store/ducks/user";
+
+export default function Login({ navigation }) {
+  const refEmail = useRef(null);
+  const refPasswordConfirm = useRef(null);
+  const refPassword = useRef(null);
+  const { users: allUsers } = useSelector(({ user }) => user);
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [loadingState, setLoading] = useState(false);
+  const [hidePassword, setHidePassword] = useState(true);
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const authLogin = async () => {
+    try {
+      // if(passwordChange.substr(passwordChange.length - 1,1) === "1" ) return console.log("numbrrr")
+
+      const userStorage = allUsers.findIndex(
+        (user) => user.name.toLowerCase() === name.toLowerCase()
+      );
+
+      if (userStorage !== -1) {
+        setLoading(false);
+        return setAlertMessage(
+          "Já existe um usuário com este nome cadastrado."
+        );
+      }
+
+      if (
+        (!name || name === "",
+        !password ||
+          password === "" ||
+          !passwordConfirm ||
+          passwordConfirm === "")
+      )
+        return setAlertMessage(
+          "Todos os campos são de preenchimento obrigatório."
+        );
+
+      if (name.length < 5)
+        return setAlertMessage("O nome deve ter no mínimo 5 caracteres.");
+
+      if (password && password.length < 5)
+        return setAlertMessage("A senha deve ter no mínimo 5 caracteres.");
+
+      if (password !== passwordConfirm)
+        return setAlertMessage("As senhas não coincidem.");
+
+      const cryptoPassword = await Crypto.digestStringAsync(
+        Crypto.CryptoDigestAlgorithm.SHA256,
+        password
+      );
+
+      dispatch(addUser({ name, cryptoPassword }));
+      setLoading(false);
+      refEmail.current.clear();
+      refPassword.current.clear();
+      refPasswordConfirm.current.clear();
+      setName("");
+      setPassword("");
+      setHidePassword(true);
+      return navigation.navigate("Login");
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      return setAlertMessage("Falha ao realizar cadastro.");
+    }
+  };
+
+  if (loadingState)
+    return (
+      <ActivityIndicator style={styles.loading} size="large" color="black" />
+    );
+
+  return (
+    <View style={styles.container}>
+      <View style={{ alignItems: "center" }}>
+        <Text style={styles.title}>Bem Vindo(a)!</Text>
+      </View>
+      <View style={styles.containerForm}>
+        <TextInput
+          ref={refEmail}
+          placeholder="Nome"
+          placeholderTextColor="black"
+          value={name}
+          style={styles.textInputEmail}
+          onChangeText={(name) => setName(name)}
+        ></TextInput>
+        <TouchableOpacity
+          style={{
+            width: "20%",
+            height: "10%",
+            resizeMode: "stretch",
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: "63%",
+            marginTop: "4%",
+          }}
+          onPress={() => setHidePassword(!hidePassword)}
+        >
+          <Entypo
+            name={hidePassword ? "eye-with-line" : "eye"}
+            size={20}
+            color="black"
+          />
+        </TouchableOpacity>
+        <TextInput
+          ref={refPassword}
+          placeholder="Senha"
+          placeholderTextColor="black"
+          secureTextEntry={hidePassword}
+          value={password}
+          style={styles.textInputPassword}
+          onChangeText={(passwordChange) => setPassword(passwordChange)}
+        ></TextInput>
+        <TextInput
+          ref={refPasswordConfirm}
+          placeholder="Repita Senha"
+          placeholderTextColor="black"
+          secureTextEntry={hidePassword}
+          value={passwordConfirm}
+          style={styles.textInputPassword}
+          onChangeText={(passwordChange) => setPasswordConfirm(passwordChange)}
+        ></TextInput>
+        <TouchableOpacity style={styles.button} onPress={authLogin}>
+          <Text style={styles.textButton}>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
+      <CustomAlert
+        show={alertMessage !== ""}
+        message={alertMessage}
+        color="#ea552b"
+        onConfirmPressed={() => {
+          setAlertMessage("");
+        }}
+      />
+
+      <View style={styles.containerLogo}>
+        <Image source={logo} style={styles.logo} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  title: {
+    marginTop: "5%",
+    fontSize: 25,
+    fontWeight: "bold",
+    marginBottom: "10%",
+  },
+  containerLogo: {
+    height: "31%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "10%",
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
+  },
+  button: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 20,
+    height: 40,
+    width: 100,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textButton: {
+    color: "black",
+  },
+  textInputEmail: {
+    color: "black",
+    alignSelf: "stretch",
+    paddingBottom: 10,
+    marginLeft: 50,
+    marginBottom: "3%",
+    marginRight: 50,
+    borderBottomColor: "black",
+    borderBottomWidth: 3,
+  },
+  textInputPassword: {
+    color: "black",
+    alignSelf: "stretch",
+    paddingBottom: 10,
+    marginLeft: 50,
+    marginBottom: "8%",
+    marginRight: 50,
+    borderBottomColor: "black",
+    borderBottomWidth: 3,
+  },
+  containerForm: {
+    alignItems: "center",
+  },
+  loading: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "transparent",
+  },
+});
---
+++ src/pages/login.js
@@ -0,0 +1,267 @@
+import React, { useRef, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+  KeyboardAvoidingView,
+} from "react-native";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as Permissions from "expo-permissions";
+import * as Crypto from "expo-crypto";
+import { Entypo } from "@expo/vector-icons";
+import CustomAlert from "../components/customAlert";
+import imageBack from "../../assets/back_login.png";
+import logo from "../../assets/logo.png";
+import { setUserCurrent } from "../store/ducks/user";
+import api from "../service/api";
+import { setValues } from "../store/ducks/stockExchange";
+
+export default function Login({ navigation }) {
+  const refEmail = useRef(null);
+  const refSenha = useRef(null);
+  const { userCurrent, users } = useSelector(({ user }) => user);
+  const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [alertMessage, setAlertMessage] = useState("");
+  const [loadingState, setLoading] = useState(false);
+  const [hidePassword, setHidePassword] = useState(true);
+
+  useEffect(() => {
+    if (userCurrent) {
+      const user = users.find((u) => u.user === userCurrent);
+      if (user) setName(user.name);
+    }
+  }, [users, userCurrent]);
+
+  const getValues = async () => {
+    try {
+      const { data: high } = await api.get(
+        "/finance/stock_price?key=525d2244&symbol=get-high"
+      );
+      const { data: low } = await api.get(
+        "/finance/stock_price?key=525d2244&symbol=get-low"
+      );
+
+      if(high && low) dispatch(setValues({ high: high.results, low: low.results }));
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  confirmPermissions = async () => {
+    setLoading(true);
+    let permission = await Permissions.getAsync(Permissions.LOCATION);
+    if (permission.status !== "granted") {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== "granted") {
+        setAlertMessage(
+          "Não é possível entrar sem permitir o uso da localização. Entre nas configurações do celular e aceite a utilização da localização pelo aplicativo MaxiForest."
+        );
+        setLoading(false);
+        return false;
+      }
+    }
+    
+    return true;
+  };
+
+  const authLogin = async () => {
+    try {
+      if (!(await confirmPermissions())) return;
+
+      const cryptoPassword = await Crypto.digestStringAsync(
+        Crypto.CryptoDigestAlgorithm.SHA256,
+        password
+      );
+
+      const userStorage = users.find(
+        (user) => user.name === name && user.cryptoPassword === cryptoPassword
+      );
+
+      if (userStorage) {
+        setLoading(false);
+        return setAlertMessage("E-mail ou senha inválidos.");
+      }
+
+      getValues();
+
+      dispatch(setUserCurrent(userStorage.name));
+
+      setLoading(false);
+      refEmail.current.clear();
+      refSenha.current.clear();
+      setName("");
+      setPassword("");
+      setHidePassword(true);
+      return navigation.navigate("HomePage");
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+      return setAlertMessage("E-mail ou senha inválidos.");
+    }
+  };
+
+  if (loadingState)
+    return (
+      <ActivityIndicator style={styles.loading} size="large" color="black" />
+    );
+
+  return (
+    <ImageBackground source={imageBack} style={styles.backgroundImage}>
+      <KeyboardAvoidingView>
+        <View style={styles.containerLogo}>
+          <Image source={logo} style={styles.logo} />
+        </View>
+        <View style={styles.containerForm}>
+          <TextInput
+            ref={refEmail}
+            placeholder="Nome"
+            placeholderTextColor="black"
+            value={name}
+            style={styles.textInputEmail}
+            onChangeText={(nameChange) => setName(nameChange)}
+          ></TextInput>
+          <TouchableOpacity
+            style={{
+              width: "20%",
+              height: "10%",
+              resizeMode: "stretch",
+              justifyContent: "center",
+              alignItems: "center",
+              marginLeft: "63%",
+              marginTop: "4%",
+            }}
+            onPress={() => setHidePassword(!hidePassword)}
+          >
+            <Entypo
+              name={hidePassword ? "eye-with-line" : "eye"}
+              size={20}
+              color="black"
+            />
+          </TouchableOpacity>
+          <TextInput
+            ref={refSenha}
+            placeholder="Senha"
+            placeholderTextColor="black"
+            secureTextEntry={hidePassword}
+            value={password}
+            style={styles.textInputPassword}
+            onChangeText={(passwordChange) => setPassword(passwordChange)}
+          ></TextInput>
+          <View style={styles.columButton}>
+            <View style={styles.itemButton}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => {
+                  return navigation.navigate("Cadastro");
+                }}
+              >
+                <Text style={styles.textButton}>Casastrar</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.itemButton}>
+              <TouchableOpacity style={styles.button} onPress={authLogin}>
+                <Text style={styles.textButton}>Entrar</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <CustomAlert
+            show={alertMessage !== ""}
+            message={alertMessage}
+            color="#ea552b"
+            onConfirmPressed={() => {
+              setAlertMessage("");
+            }}
+          />
+        </View>
+
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.created}>Created by Wilson Caetano</Text>
+        </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerLogo: {
+    height: "31%",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "10%",
+  },
+  logo: {
+    width: "100%",
+    height: "100%",
+  },
+  backgroundImage: {
+    flex: 1,
+  },
+  button: {
+    marginTop: "10%",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 20,
+    height: 40,
+    width: 100,
+    backgroundColor: "transparent",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textButton: {
+    color: "black",
+  },
+  textInputEmail: {
+    color: "black",
+    alignSelf: "stretch",
+    paddingBottom: 10,
+    marginLeft: 50,
+    marginBottom: "3%",
+    marginRight: 50,
+    borderBottomColor: "black",
+    borderBottomWidth: 3,
+  },
+  textInputPassword: {
+    color: "black",
+    alignSelf: "stretch",
+    paddingBottom: 10,
+    marginLeft: 50,
+    marginBottom: "3%",
+    marginRight: 50,
+    borderBottomColor: "black",
+    borderBottomWidth: 3,
+  },
+  containerForm: {
+    alignItems: "center",
+  },
+  textCheck: {
+    color: "black",
+  },
+  loading: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "transparent",
+  },
+  created: {
+    color: "gray",
+  },
+  itemButton: {
+    alignItems: "center",
+    width: "40%",
+  },
+  columButton: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start",
+  },
+});
