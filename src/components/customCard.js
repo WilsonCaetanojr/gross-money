64a134266fcae4d3f4985cc3db5379e8874a98c3
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-09-13 12:12:47 -0300

    Passa a utilizar hooks em todo o componente que antes era uma classe

--- src/components/customCard.js
+++ src/components/customCard.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AppLoading } from "expo";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Container,
   Card,
@@ -16,90 +17,111 @@ import {
 } from "native-base";
 import logoLow from "../../assets/logo_low.png";
 import logoHigh from "../../assets/logo_high.png";
+import { addFavorites, removeFavorites } from "../store/ducks/stockExchange";
 
-export default class CustomCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isReady: false,
-      favorite: false,
-      update: this.props.updatedAt,
+const CustomCard = ({
+  updatedAt,
+  high,
+  symbol,
+  changePercent,
+  currency,
+  price,
+  name,
+}) => {
+  const userCurrent = useSelector(({ user }) => user.userCurrent);
+  const favoritesStorage = useSelector(
+    ({ stockExchange }) => stockExchange.favorites
+  );
+  const [favorite, setFavorite] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getFont = async () => {
+      await Font.loadAsync({
+        Roboto: require("native-base/Fonts/Roboto.ttf"),
+        Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+        ...Ionicons.font,
+      });
+      setIsReady(true);
     };
-  }
+    getFont();
+  }, []);
 
-  async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true });
-  }
-
-  render() {
-    if (!this.state.isReady) {
-      return <AppLoading />;
-    }
-
-    return (
-      <Container
-        style={{ height: 100, width: "95%", backgroundColor: "#00214D" }}
-      >
-        <Card>
-          <CardItem>
-            <Left>
-              <Thumbnail source={this.props.high ? logoHigh : logoLow} />
-              <Body>
-                <Text>{this.props.high ? "Alta" : "Baixa"}</Text>
-                <Text note>{this.props.symbol}</Text>
-              </Body>
-            </Left>
-          </CardItem>
-          <CardItem cardBody style={{ marginLeft: 10 }}>
-            {this.props.high ? (
-              <Text style={{ color: "green" }}>
-                Percentual de alta: {this.props.changePercent}
-              </Text>
-            ) : (
-              <Text style={{ color: "red" }}>
-                Percentual de baixa: {this.props.changePercent}
-              </Text>
-            )}
-          </CardItem>
-          <CardItem cardBody style={{ marginLeft: 10 }}>
-            <Text>
-              Preço: {this.props.price} {this.props.currency}
-            </Text>
-          </CardItem>
-          <CardItem cardBody style={{ marginLeft: 10 }}>
-            <Text>Nome: {this.props.name}</Text>
-          </CardItem>
-          <CardItem>
-            <Left>
-              <Button
-                transparent
-                onPress={() =>
-                  this.setState({ favorite: !this.state.favorite })
-                }
-              >
-                {this.state.favorite ? (
-                  <AntDesign name="star" size={30} color="black" />
-                ) : (
-                  <AntDesign name="staro" size={30} color="black" />
-                )}
-                <Text>
-                  {this.state.favorite
-                    ? "Remover dos Favoritos"
-                    : "Adicionar aos favoritos"}
-                </Text>
-              </Button>
-            </Left>
-            <Right>
-              <Text style={{ marginLeft: 50 }}>{this.state.update}</Text>
-            </Right>
-          </CardItem>
-        </Card>
-      </Container>
+  useEffect(() => {
+    const findFavorite = favoritesStorage[userCurrent.name].find(
+      (fav) => fav.symbol === symbol
     );
+    if (findFavorite) return setFavorite(true);
+
+    setFavorite(false);
+  }, [favoritesStorage, userCurrent, symbol]);
+
+  const updateFavorite = () => {
+    if (!favorite) {
+      dispatch(addFavorites(userCurrent.name, { name, symbol }));
+    } else {
+      dispatch(removeFavorites(userCurrent.name, symbol));
+    }
+  };
+
+  if (!isReady) {
+    return <AppLoading />;
   }
-}
+
+  return (
+    <Container
+      style={{ height: 100, width: "95%", backgroundColor: "#ecececad" }}
+    >
+      <Card>
+        <CardItem>
+          <Left>
+            <Thumbnail source={high ? logoHigh : logoLow} />
+            <Body>
+              <Text>{high ? "Alta" : "Baixa"}</Text>
+              <Text note>{symbol}</Text>
+            </Body>
+          </Left>
+        </CardItem>
+        <CardItem cardBody style={{ marginLeft: 10 }}>
+          {high ? (
+            <Text style={{ color: "green" }}>
+              Percentual de alta: {changePercent}
+            </Text>
+          ) : (
+            <Text style={{ color: "red" }}>
+              Percentual de baixa: {changePercent}
+            </Text>
+          )}
+        </CardItem>
+        <CardItem cardBody style={{ marginLeft: 10 }}>
+          <Text>
+            Preço: {price} {currency}
+          </Text>
+        </CardItem>
+        <CardItem cardBody style={{ marginLeft: 10 }}>
+          <Text>Nome: {name}</Text>
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Button transparent onPress={() => updateFavorite()}>
+              {favorite ? (
+                <AntDesign name="star" size={30} color="black" />
+              ) : (
+                <AntDesign name="staro" size={30} color="black" />
+              )}
+              <Text>
+                {favorite ? "Remover dos Favoritos" : "Adicionar aos favoritos"}
+              </Text>
+            </Button>
+          </Left>
+          <Right>
+            <Text style={{ marginLeft: 50 }}>{updatedAt}</Text>
+          </Right>
+        </CardItem>
+      </Card>
+    </Container>
+  );
+};
+
+export default CustomCard;
