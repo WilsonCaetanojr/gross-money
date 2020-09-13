b4b5b9a787a0700ebb2fb350f369df1d8655c6f9
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-09-13 12:16:16 -0300

    Utiliza useEffect para carregar informações do storage, e valida a exitência  das mesmas, evitando possíveis erros de delay

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import CustomCard from "../components/customCard";
 import { View } from "native-base";
+import { TouchableOpacity, Text } from "react-native";
 
 export default function HomePage() {
   const { high, low } = useSelector(({ stockExchange }) => stockExchange);
-  const [newHigh, setHigh] = useState(high[Object.keys(high)[0]]);
-  const [newLow, setLow] = useState(low[Object.keys(low)[0]]);
+  const [newHigh, setHigh] = useState(null);
+  const [newLow, setLow] = useState(null);
+
+  useEffect(() => {
+    setHigh(high[Object.keys(high)[0]]);
+    setLow(low[Object.keys(low)[0]]);
+  }, [newHigh, newLow, high, low]);
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#00214D", alignItems: "center" }}>
+    <View style={{ flex: 1, backgroundColor: "#ecececad", alignItems: "center" }}>
       <View
         style={{
           marginTop: "18%",
@@ -18,25 +24,29 @@ export default function HomePage() {
           alignItems: "center",
         }}
       >
-        <CustomCard
-          name={newHigh.name}
-          price={newHigh.price}
-          high={true}
-          symbol={newHigh.symbol}
-          currency={newHigh.currency}
-          region={newHigh.region}
-          changePercent={newHigh.change_percent}
-          updatedAt={newHigh.updated_at}
-        ></CustomCard>
-        <CustomCard
-          name={newLow.name}
-          price={newLow.price}
-          symbol={newLow.symbol}
-          currency={newLow.currency}
-          region={newLow.region}
-          changePercent={newLow.change_percent}
-          updatedAt={newLow.updated_at}
-        ></CustomCard>
+        {newHigh && newHigh.name && (
+          <>
+            <CustomCard
+              name={newHigh.name}
+              price={newHigh.price}
+              high={true}
+              symbol={newHigh.symbol}
+              currency={newHigh.currency}
+              region={newHigh.region}
+              changePercent={newHigh.change_percent}
+              updatedAt={newHigh.updated_at}
+            ></CustomCard>
+            <CustomCard
+              name={newLow.name}
+              price={newLow.price}
+              symbol={newLow.symbol}
+              currency={newLow.currency}
+              region={newLow.region}
+              changePercent={newLow.change_percent}
+              updatedAt={newLow.updated_at}
+            ></CustomCard>
+          </>
+        )}
       </View>
     </View>
   );
