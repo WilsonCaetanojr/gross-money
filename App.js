e27754d8fe86616c76ffda5894cc38c72895dea9
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-09-13 10:15:04 -0300

    Modifica estilo do cabeçalho e adiciona novas dependências ao projeto

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, StyleSheet, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Login from "./src/pages/login";
 import Cadastro from "./src/pages/cadastro";
-import HomePage from "./src/pages/homePage";
+import BottomNav from "./src/components/bottomNavigation";
 import { persistor, store } from "./src/store/store";
-
-const styleHeaderLogin = {
-  title: "",
-  headerStyle: {
-    height: 0,
-    backgroundColor: "white",
-  },
-};
-
-const styleHeaderGeneric = {
-  title: "",
-  headerStyle: {
-    height: 30,
-    backgroundColor: "white",
-  },
-};
+import header from "./assets/header.png";
 
 const Stack = createStackNavigator();
 
@@ -31,26 +16,48 @@ export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <StatusBar backgroundColor="white" />
+        <StatusBar backgroundColor="black" />
         <NavigationContainer>
           <Stack.Navigator initialRouteName="Login" headerMode="screen">
+            <Stack.Screen
+              name="HomePage"
+              options={{
+                title: "",
+                headerBackTitle: null,
+                headerTintColor: "#fff",
+                headerTitleStyle: {
+                  fontWeight: "bold",
+                },
+                headerTransparent: true,
+                headerBackground: () => (
+                  <Image source={header} style={styles.header} />
+                ),
+              }}
+              component={BottomNav}
+            ></Stack.Screen>
             <Stack.Screen
               name="Login"
               headerMode="none"
               component={Login}
-              options={styleHeaderLogin}
+              options={{
+                title: "",
+                headerStyle: {
+                  height: 0,
+                  backgroundColor: "white",
+                },
+              }}
             />
             <Stack.Screen
               name="Cadastro"
               headerMode="none"
               component={Cadastro}
-              options={styleHeaderGeneric}
-            />
-            <Stack.Screen
-              name="HomePage"
-              headerMode="none"
-              component={HomePage}
-              options={styleHeaderGeneric}
+              options={{
+                title: "",
+                headerStyle: {
+                  height: 45,
+                  backgroundColor: "white",
+                },
+              }}
             />
           </Stack.Navigator>
         </NavigationContainer>
@@ -58,3 +65,10 @@ export default function App() {
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    height: "100%",
+    width: "100%",
+  },
+});
